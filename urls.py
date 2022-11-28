8fc468cae8d4f4b9804f9487122de6822b7c0cb7
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-11-28 15:08:48 +0100

    Updated urls and api

--- urls.py
+++ urls.py
@@ -3,20 +3,24 @@ from rest_framework import routers
 from . import views
 import diana.utils as utils
 
-router = routers.DefaultRouter()
-router.register(r'api/country', views.CountryViewSet, basename='country')
-router.register(r'api/province', views.ProvinceViewSet, basename='province')
-router.register(r'api/parish', views.ParishViewSet, basename='parish')
-router.register(r'api/lau', views.LAUViewSet, basename='lau')
-router.register(r'api/nuts1', views.NUTS1ViewSet, basename='nuts1')
-router.register(r'api/nuts2', views.NUTS2ViewSet, basename='nuts2')
-router.register(r'api/nuts3', views.NUTS3ViewSet, basename='nuts3')
 
+router = routers.DefaultRouter()
+endpoint = utils.build_app_endpoint("geography")
+documentation = utils.build_app_api_documentation("geography", endpoint)
+
+
+router = routers.DefaultRouter()
+router.register(f'{endpoint}/country', views.CountryViewSet, basename='country')
+router.register(f'{endpoint}/province', views.ProvinceViewSet, basename='province')
+router.register(f'{endpoint}/parish', views.ParishViewSet, basename='parish')
+router.register(f'{endpoint}/lau', views.LAUViewSet, basename='lau')
+router.register(f'{endpoint}/nuts1', views.NUTS1ViewSet, basename='nuts1')
+router.register(f'{endpoint}/nuts2', views.NUTS2ViewSet, basename='nuts2')
+router.register(f'{endpoint}/nuts3', views.NUTS3ViewSet, basename='nuts3')
 
 
 urlpatterns = [
     path('', include(router.urls)),
 
-    # Automatically generated views
-    # *utils.get_model_urls('geography', 'api', exclude=['province', 'parish', 'country', 'lau', 'nuts1', 'nuts2', 'nuts3']),
+    *documentation,
 ]
